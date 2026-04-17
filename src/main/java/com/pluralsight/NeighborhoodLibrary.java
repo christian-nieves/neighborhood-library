8a0876e2fe03c2ecff50390d8d193d21babcb110
christian-nieves <thechristiannieves@gmail.com>
2026-04-17 15:07:42 -0400

    Made changes to Book and started assigning books in array

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class NeighborhoodLibrary {
+    private static Book[] books = new Book[20]; // An array of books that goes up to 20
+
+    private static int numBooks = 20; // Amount of books in array
+
+    public static void main(String[] args) {
+
+        books[0] = new Book(001, "B-123-01", "Lord of the rings");
+        System.out.println(books[0]);
+    }
 }
